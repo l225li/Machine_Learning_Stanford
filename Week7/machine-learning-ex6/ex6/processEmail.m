3995aa176a96ce672969b0e2dd5f171d436ecd60
Lin Li <Lin@Lins-MacBook-Pro.local>
2016-12-11 16:56:46 +0100

    Finished Email Processing which maps each word in given email content into a vector of word indices in the prestored vocabulary list (if the word exists in vocabList, if not, it's ignored).

--- Week7/machine-learning-ex6/ex6/processEmail.m
+++ Week7/machine-learning-ex6/ex6/processEmail.m
@@ -97,11 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
+    index = find(strcmp(vocabList,str));
+    if(size(index,2) ~= 0),
+        word_indices = [word_indices;index];
+    end;
 
 
 
